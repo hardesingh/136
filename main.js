0cb1081bd194674fdb776661a6077b66b89d95da
hardesingh <96874213+hardesingh@users.noreply.github.com>
2022-10-28 07:08:38 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+video = "";
+status = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    video.hide();
+    canvas.center();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"; 
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
